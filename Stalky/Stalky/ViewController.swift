ca90ed25d3fc23b8e0b67a46210f1b9302e1a7c2
Jana Pejić <jana.pejic@quartett-mobile.de>
2017-11-17 20:01:02 +0100

    Add empty project

---
+++ Stalky/Stalky/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Stalky
+//
+//  Created by Jana Pejić on 17.11.17..
+//  Copyright © 2017. Jana Pejić. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
