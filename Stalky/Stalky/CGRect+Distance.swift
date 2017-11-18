c603c16f509b3ee5c969d7eebf6142287a1f40c0
Jana Pejić <jana.pejic.9@gmail.com>
2017-11-18 14:38:47 +0100

    Move to new file

---
+++ Stalky/Stalky/CGRect+Distance.swift
@@ -0,0 +1,22 @@
+//
+//  CGRect+Distance.swift
+//  Stalky
+//
+//  Created by Jana Pejić on 18.11.17..
+//  Copyright © 2017. Jana Pejić. All rights reserved.
+//
+
+import UIKit
+
+extension CGRect {
+
+    func distance(to other: CGRect) -> CGFloat {
+        let paths: [KeyPath<CGRect, CGFloat>] = [
+            \.origin.x,
+            \.origin.y,
+            ]
+        let differences = paths.map { abs(self[keyPath: $0] - other[keyPath: $0]) }
+        return differences.reduce(0.0) { $0 + $1 }
+    }
+
+}
