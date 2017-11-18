f3039c133a15112ef1cf1b524763767df78d8801
Mathias Quintero <me@quintero.io>
2017-11-18 14:23:33 +0100

    Adding FacebookAuth to every request

--- Stalky/Stalky/API.swift
+++ Stalky/Stalky/API.swift
@@ -9,18 +9,39 @@
 import Sweeft
 import FacebookCore
 
+struct FacebookAuth {
+    let accessToken: AccessToken
+}
+
+extension FacebookAuth: Auth {
+    
+    func apply(to request: URLRequest) -> Promise<URLRequest, APIError> {
+        var request = request
+        request.addValue(accessToken.authenticationToken, forHTTPHeaderField: "Authorization")
+        return .successful(with: request)
+    }
+    
+}
+
 struct StalkyAPI: API {
     
     enum Endpoint: String, APIEndpoint {
-        case user
+        case user = "user/"
         case find
     }
     
     static var shared: StalkyAPI {
-        return .init(baseURL: "http://swifty.cloudapp.net/api")
+        return .init(baseURL: "http://165.227.130.27/api")
     }
     
     let baseURL: String
+    
+    var auth: Auth {
+        guard let accessToken = AccessToken.current else {
+            return NoAuth.standard
+        }
+        return FacebookAuth(accessToken: accessToken)
+    }
 }
 
 extension StalkyAPI {
@@ -32,7 +53,6 @@ extension StalkyAPI {
         }
         let body: JSON = [
             "id": accessToken.userId,
-            "access_token": accessToken.authenticationToken,
         ]
         return doJSONRequest(with: .post, to: .user, body: body)
     }
